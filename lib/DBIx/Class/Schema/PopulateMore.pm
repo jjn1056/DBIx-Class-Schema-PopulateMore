4706f6b8f0201f3e60ccc4bf224689cf19651e4f
jnapiorkowski <jnapiorkowski@bd8105ee-0ff8-0310-8827-fb3f25b6796d>
2009-06-25 21:47:07 +0000

    added SQLT to the makefile so that the tests pass properly
    git-svn-id: http://dev.catalyst.perl.org/repos/bast/DBIx-Class-Schema-PopulateMore/truck@6791 bd8105ee-0ff8-0310-8827-fb3f25b6796d

--- lib/DBIx/Class/Schema/PopulateMore.pm
+++ lib/DBIx/Class/Schema/PopulateMore.pm
@@ -11,11 +11,11 @@ DBIx::Class::Schema::PopulateMore - An enhanced populate method
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
