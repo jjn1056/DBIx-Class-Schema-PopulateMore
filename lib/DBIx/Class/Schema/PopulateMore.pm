0304734deb3ae08083fbcf6b26aa5e58a24b2f21
jnapiorkowski <jnapiorkowski@bd8105ee-0ff8-0310-8827-fb3f25b6796d>
2009-06-29 21:49:25 +0000

    prep for yet another release to fix baffling CPAN test fails
    git-svn-id: http://dev.catalyst.perl.org/repos/bast/DBIx-Class-Schema-PopulateMore/truck@6858 bd8105ee-0ff8-0310-8827-fb3f25b6796d

--- lib/DBIx/Class/Schema/PopulateMore.pm
+++ lib/DBIx/Class/Schema/PopulateMore.pm
@@ -11,11 +11,11 @@ DBIx::Class::Schema::PopulateMore - An enhanced populate method
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 =head1 SYNOPSIS
 
