d87763c6a433dd41d7a2787e5c3e9c377d24044f
jnapiorkowski <jnapiorkowski@bd8105ee-0ff8-0310-8827-fb3f25b6796d>
2009-06-29 14:04:13 +0000

    updated version strings
    git-svn-id: http://dev.catalyst.perl.org/repos/bast/DBIx-Class-Schema-PopulateMore/truck@6845 bd8105ee-0ff8-0310-8827-fb3f25b6796d

--- lib/DBIx/Class/Schema/PopulateMore.pm
+++ lib/DBIx/Class/Schema/PopulateMore.pm
@@ -11,11 +11,11 @@ DBIx::Class::Schema::PopulateMore - An enhanced populate method
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 =head1 SYNOPSIS
 
